106e36ba63d145e1f4ebcca9f5b18f4315b19726
nilstop <nilstopgaard@gmail.com>
2026-07-12 23:01:41 +0200

    added bee sprite to enemy and more PARTICLES

--- minigame/Generation/Enemy/enemy.gd
+++ minigame/Generation/Enemy/enemy.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
-@onready var color_rect: ColorRect = $ColorRect
 @onready var score_label := get_tree().get_first_node_in_group("score_label")
-@onready var death_particle: CPUParticles2D = $Death_Particle
-@onready var death_particle_2: CPUParticles2D = $Death_Particle2
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var fx_sprite: Sprite2D = $FxSprite
 @onready var camera_2d: Camera2D = get_tree().get_first_node_in_group("camera")
+
+@onready var fx_sprite: Sprite2D = $FxSprite
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+# Particle references
+@onready var death_particle_2: CPUParticles2D = $Death_Particle2
+@onready var death_particle: CPUParticles2D = $Death_Particle
+@onready var wing: CPUParticles2D = $BodyExplode/Wing
+@onready var sting: CPUParticles2D = $BodyExplode/Sting
+@onready var head: CPUParticles2D = $BodyExplode/Head
+
 
 var start_y_pos: float
 var bounce_offset := 0.0
@@ -16,6 +23,7 @@ var bounce_freq := 1.0
 
 func _ready() -> void:
 	start_y_pos = global_position.y
+	sprite.frame = randi_range(0, 6)
 
 func _physics_process(_delta: float) -> void:
 	global_position.x -= Global.speed
@@ -32,21 +40,20 @@ func _on_area_entered(area: Area2D) -> void:
 		monitorable = false
 		#collision_shape.disabled = true
 		collision_shape.queue_free()
-		#var color = color_rect.color
-		#color_rect.color = Color.WHITE
+		#var color = sprite.color
+		#sprite.color = Color.WHITE
 		area.queue_free()
-		death_particle.emitting = true
-		death_particle_2.emitting = true
+		particles()
 		#for i in 3:
-		#	color_rect.position = Vector2(randi_range(-8,-0), randi_range(-8,-0))
+		#	sprite.position = Vector2(randi_range(-8,-0), randi_range(-8,-0))
 		#	await get_tree().create_timer(0.07).timeout
-		#	color_rect.color = color
+		#	sprite.color = color
 		Global.score += 20
 		score_label.text = str(Global.score)
-		color_rect.hide()
+		sprite.hide()
 
 		#get_tree().get_first_node_in_group("camera").shake(3)
-		await death_particle.finished
+		await head.finished
 		queue_free()
 		
 	elif area.is_in_group("barrier"):
@@ -55,6 +62,13 @@ func _on_area_entered(area: Area2D) -> void:
 		Global.is_game_over = true
 		Global.emit_signal("game_over")
 		Global.speed = 0
+
+func particles():
+	death_particle.emitting = true
+	death_particle_2.emitting = true
+	wing.emitting = true
+	sting.emitting = true
+	head.emitting = true
 
 #func inst(scene):
 	#var instance = scene.instantiate()
